0d22ad6dfda057b88dfbf3a99ae6f419c64e04c0
Vaibhav Gupta <79602253+vaibhavdevelper@users.noreply.github.com>
2025-09-10 11:39:17 +0000

    config

--- start/jscode.js
+++ start/jscode.js
@@ -1,1 +1,13 @@
 console.log("open terminal run");
+console.log("terminal1");
+console.log("terminal1");
+console.log("terminal1");
+console.log("terminal1");
+console.log("terminal1");
+
+console.log("terminal1");
+console.log("vaibhav");
+console.log("javscript");
+console.log("terminal1");
+console.log("terminal1");
+console.log("terminal1");
